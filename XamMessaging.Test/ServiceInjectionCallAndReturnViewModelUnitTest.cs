02ec1ce494d5444913395b178161155a5a921a88
Rodrigo Juarez <rodrigomjuarez@gmail.com>
2018-09-09 09:47:32 -0300

    Using moq for testing the vm with services

--- XamMessaging.Test/ServiceInjectionCallAndReturnViewModelUnitTest.cs
+++ XamMessaging.Test/ServiceInjectionCallAndReturnViewModelUnitTest.cs
@@ -8,6 +8,7 @@ namespace XamMessaging.Test
     [TestClass]
     public class ServiceInjectionCallAndReturnViewModelUnitTest
     {
+
         [TestMethod]
         public void CanCreateViewModelOk()
         {
@@ -20,5 +21,43 @@ namespace XamMessaging.Test
             // Assert
             Assert.IsNotNull(vm);
         }
+
+        [TestMethod]
+        public void CanConfirmExecutionOfSomeOperation()
+        {
+            // Arrange
+            var service = new Mock<IConfirmationService>();
+            service.Setup(m => m.AskConfirmation(It.IsAny<string>())).ReturnsAsync(true);
+
+            var vm = new ServiceInjectionCallAndReturnViewModel(service.Object);
+
+            var startingNumberOfOperation = vm.Operations.Count;
+
+            // Act
+            vm.ExecuteSomeOperationCommand.Execute(null);
+
+            // Assert
+            Assert.AreEqual(startingNumberOfOperation + 1, vm.Operations.Count);
+        }
+
+
+        [TestMethod]
+        public void CanCancelExecutionOfSomeOperation()
+        {
+            // Arrange
+            var service = new Mock<IConfirmationService>();
+            service.Setup(m => m.AskConfirmation(It.IsAny<string>())).ReturnsAsync(false);
+
+            var vm = new ServiceInjectionCallAndReturnViewModel(service.Object);
+
+            var startingNumberOfOperation = vm.Operations.Count;
+
+            // Act
+            vm.ExecuteSomeOperationCommand.Execute(null);
+
+            // Assert
+            Assert.AreEqual(startingNumberOfOperation, vm.Operations.Count);
+        }
+
     }
 }
